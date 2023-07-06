15ef1203f2a43037f9232a93b89e1fd3980193c7
Soumil <soumil096@duck.com>
2023-07-07 00:21:56 +0530

    Initial Commit

---
+++ HackerRank/30 Days of Python/Day1.py
@@ -0,0 +1,15 @@
+# Declare second integer, double, and String variables.
+newi = int(input())
+newd = float(input())
+news = input()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(i + newi)
+print(d + newd)
+print(s + news)
